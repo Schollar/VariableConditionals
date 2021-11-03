2e4d006f11ed3216d7365a37dce5d501fc37f864
schollar <cschollar22@gmail.com>
2021-11-02 20:50:43 -0600

    Changed into a function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
-var age = 17;
-var is_subscribed = false;
+// var age = 17;
+// var is_subscribed = false;
 
+
+function Userinfo(age, is_subscribed) {
 
 if(age >= 18 && is_subscribed === true) {
-    console.log("User is above the age of 18 and is Subscribed");
+    console.log("User is 18 or older and is Subscribed");
 } else if(age < 18 && is_subscribed === true) {
     console.log("User is under the age of 18 and is Subscribed");
 } else if(age >= 18 && is_subscribed != true) {
-    console.log("User is over 18 but is not Subscribed");
+    console.log("User is 18 or older but is not Subscribed");
 } else if(age < 18 && is_subscribed != true) {
     console.log("User is under 18 and not subscribed");
 } else {
     console.log ("Invalid User data given");
 }
+
+}
+
+Userinfo( 19, true);
+
+Userinfo (15, false);
