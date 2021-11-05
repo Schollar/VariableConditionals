1bc20482f8cce622002c74e6f7f8f18734dd1d53
schollar <cschollar22@gmail.com>
2021-11-04 20:58:19 -0600

    Changed variables into arrays, added for loop to loop through the arrays

--- js/app.js
+++ js/app.js
@@ -1,27 +1,65 @@
-// var age = 17;
-// var is_subscribed = false;
+// Setup arrays and variables
+var userage = [19, 15, 19, 15];
+var is_subscribed = [true, false, false, true];
+var agelength = userage.length;
 
 
-function Userinfo(age, is_subscribed) {
+// Setup for loop that runs through each item in the array and checks to see if subscribed, then console logs the appropriate message
+for (var i = 0; i < agelength; i++) {
 
-    if (age >= 18 && is_subscribed) {
+    if (userage[i] >= 18 && is_subscribed[i]) {
         console.log("User is 18 or older and is Subscribed");
-    } else if (age < 18 && is_subscribed) {
+    } else if (userage[i] < 18 && is_subscribed[i]) {
         console.log("User is under the age of 18 and is Subscribed");
-    } else if (age >= 18 && !is_subscribed) {
+    } else if (userage[i] >= 18 && !is_subscribed[i]) {
         console.log("User is 18 or older but is not Subscribed");
-    } else if (age < 18 && !is_subscribed) {
+    } else if (userage[i] < 18 && !is_subscribed[i]) {
         console.log("User is under 18 and not subscribed");
     } else {
         console.log("Invalid User data given");
     }
-
 }
 
-Userinfo(19, true);
+    // function calculator(a, b, sign) {
+    //     if (typeof (a) !== "number" || typeof (b) !== "number") {
+    //         return "unknown value";
+    //     } else {
 
-Userinfo(15, false);
+    //         if (sign === "+") {
+    //             return a + b;
+    //         } else if (sign === "-") {
+    //             return a - b;
+    //         } else if (sign === "*") {
+    //             return a * b;
+    //         }
+    //         else if (sign === "/") {
+    //             return a / b;
+    //         } else {
+    //             return "unknown value";
+    //         }
+    //     }
+    // }
 
-Userinfo(19, false);
 
-Userinfo(15, true);
+
+    // calculator(1, 4, "+");
+    // calculator(true, 2, "-");
+
+//     function between(a, b) {
+//         // your code here
+//         let c = [];
+
+//         while (true) {
+
+//             c.push(a);
+//             a = a + 1;
+//             if (a === b + 1)
+//                 return c
+//         }
+//     }
+
+
+
+
+
+// between(1, 4);
